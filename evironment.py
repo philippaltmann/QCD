d84fe12fc54a7901b7e5ea2431a56e4c4c4569dd
adelina-b <116336470+adelina-b@users.noreply.github.com>
2023-05-10 13:51:14 +0200

    started

--- evironment.py
+++ evironment.py
@@ -1,29 +1,51 @@
 import gymnasium as gym
-import numpy as np
-from gymnasium import spaces
+from gymnasium.spaces import Tuple, Box, Discrete
 
+import pennylane as qml
+from pennylane import numpy as np
 
-class Circuit(gym.Env):
-    """Quantum Ciruit Environment. Description will follow..."""
+class CircuitDesigner(gym.Env):
+    """Quantum Circuit Environment. Description will follow..."""
 
     metadata = {"render_modes": ["human"], "render_fps": 30}
 
-    def __init__(self, arg1, arg2):
+    def __init__(self, max_qubits, max_depth):
         super().__init__()
-        # Define action and observation space
-        # They must be gym.spaces objects
-        # Example when using discrete actions:
-        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
-        # Example for using image as input (channel-first; channel-last also works):
-        self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+        self.qubits = max_qubits  # the (maximal) number of available qubits
+        # define action space
+        self.action_space = Tuple((Discrete(7),Discrete(max_qubits), Box(0,2*np.pi)), seed=100)
+
+        # define observation space
+        self.observation_space =
+
+    def _action_to_operation(self, action):
+        if action[0] == 0:
+            return qml.RZ(phi=action[2],wires=action[1])
+
+    def _build_circuit(self, operations):
+
+
 
     def step(self, action):
+
+        # determining what action to take
+        if action[0] == 6:
+            terminated = True
+        else:
+            terminated = False
+            operation = self._action_to_operation(action)
+
+        # determining reward of action (only if terminated or truncated)
+        if not terminated:
+            reward = 0
+        else:
 
         return observation, reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
-
+        # start with an empty trajectory
+        self._operations = []
         return observation, info
 
     def render(self):

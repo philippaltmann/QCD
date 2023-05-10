6d4ebcae22ff0221ddc78139db81afebccb37b04
adelina-b <116336470+adelina-b@users.noreply.github.com>
2023-05-10 15:43:00 +0200

    done with action space

--- evironment.py
+++ evironment.py
@@ -14,14 +14,29 @@ class CircuitDesigner(gym.Env):
 
         self.qubits = max_qubits  # the (maximal) number of available qubits
         # define action space
-        self.action_space = Tuple((Discrete(7),Discrete(max_qubits), Box(0,2*np.pi)), seed=100)
+        self.action_space = Tuple((Discrete(5),Discrete(max_qubits), Box(low=0,high=2*np.pi,shape=(2,))), seed=100)
 
         # define observation space
         self.observation_space =
 
     def _action_to_operation(self, action):
-        if action[0] == 0:
-            return qml.RZ(phi=action[2],wires=action[1])
+        wire = action[1]
+        if action[0] == 0: # Z-Rotation
+            return qml.RZ(phi=action[2][0],wires=wire)
+        elif action[0] == 1: # Phased-X
+            op_z_p = qml.exp(qml.PauliZ(wire), 1j*action[2][1])
+            op_x = qml.exp(qml.PauliX(wire), 1j*action[2][0])
+            op_z_m = qml.exp(qml.PauliZ(wire), -1j*action[2][1])
+            return qml.prod(op_z_p,op_x,op_z_m)
+        elif action[0] == 2: # CNOT (only neighbouring qubits)
+            # decide control qubit based on action parameters
+            if action[2][0] <= action[2][1]:
+                return qml.CNOT((wire-1)%(self.qubits-1)+1, wire)
+            else:
+                return qml.CNOT((wire+1)%(self.qubits-1)-1, wire)
+        elif action[0] == 3: # mid-circuit measurement
+            return qml.measure(wire)
+
 
     def _build_circuit(self, operations):
 
@@ -30,7 +45,7 @@ class CircuitDesigner(gym.Env):
     def step(self, action):
 
         # determining what action to take
-        if action[0] == 6:
+        if action[0] == 4:
             terminated = True
         else:
             terminated = False
@@ -38,7 +53,7 @@ class CircuitDesigner(gym.Env):
 
         # determining reward of action (only if terminated or truncated)
         if not terminated:
-            reward = 0
+            reward = 0 # or -1 to punish step count?
         else:
 
         return observation, reward, terminated, truncated, info

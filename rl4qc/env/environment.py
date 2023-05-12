74a89ed12c67a6817b932e3ebad12220ed9141e5
adelina-b <116336470+adelina-b@users.noreply.github.com>
2023-05-12 14:31:09 +0200

    this almost works now... more fixes have to be done concerning pennylane and circuit construction from given actions

--- rl4qc/env/environment.py
+++ rl4qc/env/environment.py
@@ -5,6 +5,7 @@ import pennylane as qml
 # from pennylane import numpy as np
 import numpy as np
 
+
 class CircuitDesigner(gym.Env):
     """Quantum Circuit Environment. Description will follow..."""
 
@@ -19,35 +20,38 @@ class CircuitDesigner(gym.Env):
         # initialize quantum device to use for QNode
         self.device = qml.device('default.qubit', wires=max_qubits)
         # define action space
-        self.action_space = Tuple((Discrete(5),Discrete(max_qubits), Box(low=0,high=2*np.pi,shape=(2,))))
+        self.action_space = Tuple((Discrete(5), Discrete(max_qubits), Box(low=0, high=2*np.pi, shape=(2,))))
         # define observation space
         self.observation_space = Dict(
-            {'real': Box(low=np.NINF, high=np.inf, shape=(2**max_qubits,)),
-            'imag': Box(low=np.NINF, high=np.inf, shape=(2**max_qubits,))} )
+            {'real': Box(low=-1, high=+1, shape=(2**max_qubits,)),
+             'imag': Box(low=-1, high=+1, shape=(2**max_qubits,))})
 
     def _action_to_operation(self, action):
-        """ Converter translating actions from action_space into quantum operations """
+        """ Action Converter translating values from action_space into quantum operations """
         wire = action[1]
-        if action[0] == 0: # Z-Rotation
-            return qml.RZ(phi=action[2][0],wires=wire)
-        elif action[0] == 1: # Phased-X
+        if action[0] == 0:  # Z-Rotation
+            return qml.RZ(phi=action[2][0], wires=wire)
+        elif action[0] == 1:  # Phased-X
             op_z_p = qml.exp(qml.PauliZ(wire), 1j*action[2][1])
             op_x = qml.exp(qml.PauliX(wire), 1j*action[2][0])
             op_z_m = qml.exp(qml.PauliZ(wire), -1j*action[2][1])
-            return qml.prod(op_z_p,op_x,op_z_m)
-        elif action[0] == 2: # CNOT (only neighbouring qubits)
+            return qml.prod(op_z_p, op_x, op_z_m)
+        elif action[0] == 2:  # CNOT (only neighbouring qubits)
             # decide control qubit based on action parameters
             if action[2][0] <= action[2][1]:
-                return qml.CNOT((wire-1)%(self.qubits-1)+1, wire)
+                return qml.CNOT(wires=[(wire-1) % (self.qubits-1)+1, wire])
             else:
-                return qml.CNOT((wire+1)%(self.qubits-1)-1, wire)
-        elif action[0] == 3: # mid-circuit measurement
-            return qml.measure(wire)
+                return qml.CNOT(wires=[(wire+1) % (self.qubits-1)-1, wire])
+        elif action[0] == 3:  # mid-circuit measurement
+            return int(wire)
 
     def _build_circuit(self):
         """ Quantum Circuit Function taking a list of quantum operations and returning state information """
         for op in self._operations:
-            qml.apply(op)
+            if type(op) == int:
+                qml.measure(op)
+            else:
+                qml.apply(op)
         return qml.state()
 
     def _get_info(self):
@@ -55,18 +59,20 @@ class CircuitDesigner(gym.Env):
         return qml.specs(circuit)()
 
     def _draw_circiut(self):
-        circuit = qml.QNode(self._build_circuit(),self.device)
+        circuit = qml.QNode(self._build_circuit(), self.device)
         print(qml.draw(circuit)())
         # TODO: fix qml.draw_mpl(self._build_circuit())
 
     def reset(self, seed=None, options=None):
+        # set seed for random number generator
+        super().reset(seed=seed)
 
         # start with an empty trajectory of operations
         self._operations = []
         # calculate zero-state information
         circuit = qml.QNode(self._build_circuit, self.device)
-        self._observation = Dict({'real': np.real(np.array(circuit(), np.float32)),
-                                  'imag': np.imag(np.array(circuit(), np.float32))})
+        self._observation = {'real': np.real(np.array(circuit(), np.float32)),
+                             'imag': np.imag(np.array(circuit(), np.float32))}
         observation = self._observation
 
         # evaluate additional information
@@ -94,16 +100,16 @@ class CircuitDesigner(gym.Env):
             self._operations.append(operation)
             # compute state observation
             circuit = qml.QNode(self._build_circuit, self.device)
-            self._observation = Dict({'real': np.real(np.array(circuit(), np.float32)),
-                                      'imag': np.imag(np.array(circuit(), np.float32))})
+            self._observation = {'real': np.real(np.array(circuit(), np.float32)),
+                                 'imag': np.imag(np.array(circuit(), np.float32))}
 
         observation = self._observation
 
         # sparse reward of action
         if not terminated:
-            reward = 0 # or -1 to punish step count?
+            reward = 0  # or -1 to punish step count?
         else:
-            self._draw_circiut() # render circuit only after each episode
+            self._draw_circiut()  # render circuit only after each episode
             # TODO: figure out how to include reward function into this class
             reward = None
 
@@ -112,11 +118,8 @@ class CircuitDesigner(gym.Env):
 
         return observation, reward, terminated, truncated, info
 
-
     def render(self):
         return None
 
     # def close(self):
     # currently there is no need for a close method
-
-

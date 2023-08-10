0d18ccd9082472ad1d24563e3299e082bef84b3b
Philipp <philipp.altmann@ifi.lmu.de>
2023-08-10 02:09:59 +0200

    added td3 baseline

---
+++ baselines/td3.py
@@ -0,0 +1,8 @@
+from algorithm import TrainableAlgorithm
+from stable_baselines3.td3 import TD3 as StableTD3
+
+class TD3(TrainableAlgorithm, StableTD3):
+  """A Trainable extension to PPO"""
+  def __init__( self, train_freq= (4, "step"), **kwargs):
+    self.n_steps = train_freq[0] # For unified logging
+    super().__init__(train_freq=train_freq, **kwargs)

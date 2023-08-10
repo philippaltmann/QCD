d69c9c25c4d9db39962c57707a657ab0f554c35c
Philipp <philipp.altmann@ifi.lmu.de>
2023-08-10 02:10:13 +0200

    Updated env registration

--- circuit_designer/__init__.py
+++ circuit_designer/__init__.py
@@ -1,4 +1,4 @@
 from gymnasium.envs.registration import register
 
-def register_envs():
-  register(id="CircuitDesigner-v0", entry_point="circuit_designer.env:CircuitDesigner")
+# def register_envs():
+register(id="CircuitDesigner-v0", entry_point="circuit_designer.env:CircuitDesigner")

f72596b05da414034fb3568c7f2cc7e61252fca4
Philipp <philipp.altmann@ifi.lmu.de>
2023-08-12 22:45:14 +0200

    Added discrete mode to wrapper

--- circuit_designer/wrappers/monitor.py
+++ circuit_designer/wrappers/monitor.py
@@ -1,5 +1,4 @@
-import time; import numpy as np; import warnings
-
+import time; import numpy as np
 import gymnasium as gym
 from gymnasium.core import ActType, ObsType; 
 from typing import SupportsFloat
@@ -7,17 +6,19 @@ from typing import SupportsFloat
 class Monitor(gym.Wrapper[ObsType, ActType, ObsType, ActType]):
   """ A monitor wrapper for Gym environments, it is used to know the episode reward, length, time and other data.
   :param env: The environment """
-  def __init__( self, env: gym.Env, record_video=False):
-    super().__init__(env=env); self.t_start = time.time(); 
-    self.record_video = record_video; self._frame_buffer = []
+  def __init__( self, env: gym.Env, record_video=False, rescale=True, discrete=False):
+    super().__init__(env=env); self.t_start = time.time(); self.rescale = rescale
+    if self.rescale:
+      _l = self.action_space.low; _h = self.action_space.high
+      self._rescale = lambda action: np.clip((_l+ 0.5 * (action + 1.0)) * (_h - _l), _l, _h)
+    self.record_video = record_video; self._frame_buffer = []; self.discrete = discrete
+    if discrete: self.action_space = gym.spaces.Discrete(1334*self.qubits+1)
     self.states: list = [np.ndarray]; self.actions:list = [np.ndarray]; self.rewards: list[float] = []
     self._history = lambda: {key: getattr(self,key).copy() for key in ['states','actions','rewards']}
     self._episode_returns: list[float] = []; self._termination_reasons: list[str] = []
     self._episode_lengths: list[int] = []; self._episode_times: list[float] = []; 
-    self._total_steps = 0; self.needs_reset = True
-    # Q-Metrics
-    self._ep_depths, self._ep_qubits, self._ep_operations = [], [], []
-
+    self._total_steps = 0; self.needs_reset = True; self.rescale = rescale
+    self._ep_depths, self._ep_qubits, self._ep_operations = [], [], [] # Q-Metrics
 
   def reset(self, **kwargs) -> tuple[ObsType, dict]:
     """ Calls the Gym environment reset. 
@@ -29,11 +30,31 @@ class Monitor(gym.Wrapper[ObsType, ActType, ObsType, ActType]):
     if self.record_video: self._frame_buffer.append(self.render())
     return state, info
 
+  def _discrete(self, action: ActType): 
+    """     Discrete Mapping: 
+    [0; 36*max_qubis[ -> Z-Rotation for qubit 1..max_qubit
+    [36*max_qubis ..36*max_qubis+1296*max_qubits[]Phased-X \w parameters [0..35][0..35][0..n_qubits]
+    [36*max_qubis+1296*max_qubis .. 36*max_qubis+1296*max_qubis+max_qubis*2[
+    """
+    print(action)
+    o_border = [36*self.qubits, 1332*self.qubits, 1334*self.qubits, 1334*self.qubits+1]
+    operation = 0 if action < o_border[0] else 1 if action < o_border[1] else 2 if action < o_border[2] else 3
+    print(operation)
+    w_action = action - [0, *o_border][operation]
+    wire = w_action % self.qubits if operation < 3 else 0
+    phi = 0
+    theta = 0
+
+    _discrete_action = [operation, wire, phi, theta]
+    return _discrete_action
+
   def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict]:
     """ Step the environment with the given action
     :param action: the action
     :return: observation, reward, terminated, truncated, information """
     if self.needs_reset: raise RuntimeError("Tried to step environment that needs reset")
+    if self.rescale: action = self._rescale(action)
+    # if self.discrete: action = self._discrete(action)
     state, reward, terminated, truncated, info = self.env.step(action)
     self.states.append(state); self.actions.append(action); self.rewards.append(float(reward))
     if self.record_video: self._frame_buffer.append(self.render())
@@ -43,12 +64,10 @@ class Monitor(gym.Wrapper[ObsType, ActType, ObsType, ActType]):
       self._episode_returns.append(ep_rew); 
       self._termination_reasons.append(info.pop('termination_reason'))
       self._episode_lengths.append(ep_len); self._episode_times.append(time.time() - self.t_start)
-      # print("step")
 
       ep_info['d'] = info['depth']
       ep_info['q'] = info['num_used_wires']
       ep_info['o'] = info['num_operations']
-      
       info["episode"] = ep_info
 
     self._total_steps += 1

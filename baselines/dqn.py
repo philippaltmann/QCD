4d18493f2f7cd3ee352d132d52c8c6c7c45b8865
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-09-27 10:19:28 +0200

    Run DQN Benchmark in discrete mode

--- baselines/dqn.py
+++ baselines/dqn.py
@@ -1,12 +1,10 @@
 from algorithm import TrainableAlgorithm
-from stable_baselines3 import DQN as StableDQN, HerReplayBuffer
-from circuit_designer.wrappers import Monitor
-import gymnasium as gym
+from stable_baselines3 import DQN as StableDQN
 
 class DQN(TrainableAlgorithm, StableDQN):
   """A Trainable extension to DQN using HER"""
   n_steps = 1 # For unified logging
-  def __init__(self, policy="MultiInputPolicy", replay_buffer_class = HerReplayBuffer, **kwargs):
-    super().__init__(policy=policy, replay_buffer_class=replay_buffer_class, envkwargs={'discrete':True}, **kwargs)
+  def __init__(self, policy="MultiInputPolicy", **kwargs): #replay_buffer_class = HerReplayBuffer,
+    super().__init__(policy=policy, envkwargs={'discrete':True}, **kwargs)
 
 

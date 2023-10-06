044e315ca66b2f2ee268366490316f006884fd12
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-06 20:12:13 +0200

    Updated env factory

--- circuit_designer/utils/factory.py
+++ circuit_designer/utils/factory.py
@@ -16,9 +16,9 @@ def make_vec(env, seed=None, n_envs=1, **kwargs):
   spec = lambda rank: {**named(env), 'seed': seed+rank, **kwargs}
   return DummyVecEnv([_make(**spec(i)) for i in range(n_envs)])
 
-def factory(env_spec, n_train=4, **kwags):
+def factory(env_spec, n_train=4, **kwargs):
   assert len(env_spec) > 0, 'Please specify at least one environment for training'
   test_names = ['validation', *[f'evaluation-{i}' for i in range(len(env_spec)-1)]]
-  return { 'train': make_vec(env_spec[0], n_envs=n_train, **kwags), 
-    'test': {name: make_vec(spec, render_mode='text', record_video=True, **kwags) for name, spec in zip(test_names, env_spec)}
+  return { 'train': make_vec(env_spec[0], n_envs=n_train, **kwargs), 
+    'test': {name: make_vec(spec, render_mode='text', record_video=True, **kwargs) for name, spec in zip(test_names, env_spec)}
   }   

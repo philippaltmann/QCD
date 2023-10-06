0f588be02fa375fc11d60b426e3bec51e2ccf178
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-06 20:09:25 +0200

    Add factory Circuit Designer

--- circuit_designer/__init__.py
+++ circuit_designer/__init__.py
@@ -1,4 +1,5 @@
 from gymnasium.envs.registration import register
+from .utils import factory
 
 # def register_envs():
 register(id="CircuitDesigner-v0", entry_point="circuit_designer.env:CircuitDesigner")

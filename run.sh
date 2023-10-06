638bf0de1884ed19387b011980f4f2bc67846c53
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-06 22:05:41 +0200

    FIxed runner

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 for SEED in 1 2 3 4; do  # Depth = 3xoptimal solution
-  for CHALLENGE in 'UC-hadamard-q1-d9', 'SP-bell-q2-d12', 'SP-ghz3-q3-d15', 'UC-toffoli-q3-d63'
-    for ALG in 'SAC' 'PPO' 'TD3' 'DQN'; do # 'PPO' 'SAC' 'TD3'  #'A2C'
+  for CHALLENGE in 'UC-hadamard-q1-d9' 'SP-bell-q2-d12' 'SP-ghz3-q3-d15' 'UC-toffoli-q3-d63'; do
+    for ALG in 'PPO' 'A2C' ; do # 'SAC' 'TD3' 'DQN'
           O="results/out/$CHALLENGE/$ALG"; mkdir -p "$O"
-          echo "Running $ALG in $CHALLENGE [SEED $SEED]" &> "$O/$SEED.out"
+          echo "Running $ALG in $CHALLENGE [SEED $SEED]"
           python -m train $ALG -e $CHALLENGE -s $SEED &> "$O/$SEED.out" &
           sleep 5 
         done

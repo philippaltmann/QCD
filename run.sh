26dba247f0fefcf8c284fc83aa3fad8a52f73ade
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-06 20:11:03 +0200

    Adapted training runner with new env variations

--- run.sh
+++ run.sh
@@ -1,28 +1,9 @@
-for SEED in 1 2 3 4; do
-  for ALG in 'DQN'; do
-    for ENV in 'SP-random' 'SP-bell' 'SP-ghz2' 'SP-ghz1' 'UC-random' 'UC-hadamard' 'UC-toffoli';do
-      for ETA in 4 8; do
-        for DELTA in 8; do
-          O="results/out/$ENV-$ETA-$DELTA/$ALG"; mkdir -p "$O"
-          echo "Running $ALG in $ENV ($ETA | $DETLA) [SEED $SEED]" &> "$O/$SEED.out"
-          python -m train $ALG -e $ENV-q$ETA-d$DELTA -s $SEED &> "$O/$SEED.out" &
-          sleep 5 
-        done
-      done
-    done
-  done
-done
-
-sleep 4000
-
-for SEED in 3 4; do
-  for ALG in 'SAC' 'PPO' 'TD3'; do # 'PPO' 'SAC' 'TD3' 'DQN' #'A2C'
-    for ENV in 'SP-random' 'SP-bell' 'SP-ghz2' 'SP-ghz1' 'UC-random' 'UC-hadamard' 'UC-toffoli';do
-      for ETA in 4 8; do #2
-        for DELTA in 8; do # 5, 20
-          O="results/out/$ENV-$ETA-$DELTA/$ALG"; mkdir -p "$O"
-          echo "Running $ALG in $ENV ($ETA | $DETLA) [SEED $SEED]" &> "$O/$SEED.out"
-          python -m train $ALG -e $ENV-q$ETA-d$DELTA -s $SEED &> "$O/$SEED.out" &
+for SEED in 1 2 3 4; do  # Depth = 3xoptimal solution
+  for CHALLENGE in 'UC-hadamard-q1-d9', 'SP-bell-q2-d12', 'SP-ghz3-q3-d15', 'UC-toffoli-q3-d63'
+    for ALG in 'SAC' 'PPO' 'TD3' 'DQN'; do # 'PPO' 'SAC' 'TD3'  #'A2C'
+          O="results/out/$CHALLENGE/$ALG"; mkdir -p "$O"
+          echo "Running $ALG in $CHALLENGE [SEED $SEED]" &> "$O/$SEED.out"
+          python -m train $ALG -e $CHALLENGE -s $SEED &> "$O/$SEED.out" &
           sleep 5 
         done
       done

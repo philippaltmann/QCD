f46217e9d66cde762de3b0ac216a87e94cabfc9e
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-07 17:26:18 +0200

    Enable continous control for SAC

--- baselines/sac.py
+++ baselines/sac.py
@@ -4,3 +4,5 @@ from stable_baselines3.sac import SAC as StableSAC
 class SAC(TrainableAlgorithm, StableSAC):
   """A Trainable extension to SAC"""
   n_steps = 1 # For unified logging
+  def __init__(self, **kwargs):
+    super().__init__(envkwargs={'discrete': False}, **kwargs)

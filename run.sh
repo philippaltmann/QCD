90c5a45c5a51b527966c669bc29e015c397f80ac
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 02:36:57 +0200

    Updated runner

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 for ITERATION in 0 2 4 6; do
   for RUN in 1 2; do  # Depth = 3xoptimal solution
-    for CHALLENGE in 'UC-hadamard-q1-d9' 'SP-bell-q2-d12' 'SP-ghz3-q3-d15' 'UC-toffoli-q3-d63'; do
-      for ALG in 'PPO' 'A2C' 'SAC' 'TD3'; do
+    for CHALLENGE in 'UC-hadamard-q1-d9' 'SP-bell-q2-d12' 'SP-ghz3-q3-d15' 'UC-random-q2-d12' 'SP-random-q2-d12' 'UC-toffoli-q3-d63'; do
+      for ALG in 'A2C' 'PPO' 'SAC' 'TD3'; do
         SEED=$(($ITERATION + $RUN))
         O="results/out/$CHALLENGE/$ALG"; mkdir -p "$O"
         echo "Running $ALG in $CHALLENGE [SEED $SEED]"
@@ -10,5 +10,4 @@ for ITERATION in 0 2 4 6; do
       done
     done
   done
-  sleep 4200
 done

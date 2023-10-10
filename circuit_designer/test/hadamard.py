dc094ab9650acb74ed661ab3bb5a1bc1bc37dff0
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 03:37:33 +0200

    Updated hadamard test

--- circuit_designer/test/hadamard.py
+++ circuit_designer/test/hadamard.py
@@ -1,4 +1,4 @@
-from circuit_designer import factory
+from circuit_designer import factory; import numpy as np
 
 def hadamard():
   # Test 1-qubit H
@@ -13,9 +13,9 @@ def hadamard():
   # Test 2-qubit H
   env = factory(['UC-hadamard-q2-d9'], seed=1, n_train=1)
   env['train'].reset()
-  env['train'].step([[1,0,0,3]])  
-  env['train'].step([[2,0,0,3]])
-  env['train'].step([[1,0,0,3]])  
+  env['train'].step([[1,0,0,np.pi/2]])  
+  env['train'].step([[2,0,0,np.pi/2]])
+  env['train'].step([[1,0,0,np.pi/2]])  
   reward = env['train'].step([[0,0,0,0]])[1] # Meassure
   reward = env['train'].step([[0,1,0,0]])[1] # Meassure
   assert reward == 1, f"Reward not one: {reward} "

9036d614c83fdc81f3f3838417d03faf94ccd18b
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 03:37:08 +0200

    Updated bell test

--- circuit_designer/test/bell.py
+++ circuit_designer/test/bell.py
@@ -1,23 +1,21 @@
-from circuit_designer.utils import factory
-
+from circuit_designer.utils import factory; import numpy as np
 
 def bell():
-
   env = factory(['SP-bell-q2-d12'], seed=1, n_train=1) #, **envkwargs
   env['train'].reset()
 
   # H
-  env['train'].step([[1,0,0,3]])  
-  env['train'].step([[2,0,0,3]])
-  env['train'].step([[1,0,0,3]])  
+  env['train'].step([[1,0,0,np.pi/2]])  
+  env['train'].step([[2,0,0,np.pi/2]])
+  env['train'].step([[1,0,0,np.pi/2]])  
 
   # CX
-  env['train'].step([[2,1,0,4]])
+  env['train'].step([[2,1,0,np.pi]])
 
   # M
   env['train'].step([[0,0,0,0]])
   reward = env['train'].step([[0,1,0,0]])[1]
 
-  assert reward == 1
+  assert reward == 1, reward
   print("Succeeded bell test")
 

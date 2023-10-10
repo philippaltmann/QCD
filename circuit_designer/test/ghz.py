faa9b4f05f3a3cf3c8259afd29ef366b9fda949d
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 03:37:19 +0200

    Updated ghz test

--- circuit_designer/test/ghz.py
+++ circuit_designer/test/ghz.py
@@ -1,4 +1,4 @@
-from circuit_designer.utils import factory
+from circuit_designer.utils import factory; import numpy as np
 
 def ghz():
   envs = factory(['SP-ghz3-q3-d15'], seed=1, n_train=1) #, **envkwargs
@@ -7,16 +7,16 @@ def ghz():
   env.reset()
   
   # H
-  env.step([[1,0,0,3]]); env.step([[2,0,0,3]]); env.step([[1,0,0,3]])  
+  env.step([[1,0,0,np.pi/2]]); env.step([[2,0,0,np.pi/2]]); env.step([[1,0,0,np.pi/2]])  
 
   # CX
-  env.step([[2,1,0,4]]); env.step([[2,2,1,4]])
+  env.step([[2,1,0,np.pi]]); env.step([[2,2,1,np.pi]])
 
   # M
   env.step([[0,0,0,0]]); env.step([[0,1,0,0]])
   reward = env.step([[0,2,0,0]])[1]
 
-  assert reward == 1
+  assert reward == 1, reward
   print("Succeeded GHZ test")
 
 

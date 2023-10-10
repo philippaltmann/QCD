587644f2739cb909d240c55ef90c589161c058e2
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 03:37:45 +0200

    Updated toffoli test

--- circuit_designer/test/toffoli.py
+++ circuit_designer/test/toffoli.py
@@ -1,28 +1,28 @@
-from circuit_designer.utils import factory
+from circuit_designer.utils import factory; import numpy as np
 
 def toffoli():
   env = factory(['UC-toffoli-q3-d63'], seed=1, n_train=1) #, **envkwargs
   env['train'].reset()
 
   # V
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
-  env['train'].step([[1,2,1,3]])  
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
+  env['train'].step([[1,2,1,np.pi/2]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
 
   # Cnot
-  env['train'].step([[2,1,0,3]])  
+  env['train'].step([[2,1,0,np.pi/2]])  
 
   # V-
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
-  env['train'].step([[1,2,1,1]])  
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
+  env['train'].step([[1,2,1,-np.pi/2]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
 
   # Cnot
-  env['train'].step([[2,1,0,3]])  
+  env['train'].step([[2,1,0,np.pi/2]])  
 
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
-  env['train'].step([[1,2,0,3]])  
-  env['train'].step([[1,2,2,3]]); env['train'].step([[2,2,2,3]]); env['train'].step([[1,2,2,3]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
+  env['train'].step([[1,2,0,np.pi/2]])  
+  env['train'].step([[1,2,2,np.pi/2]]); env['train'].step([[2,2,2,np.pi/2]]); env['train'].step([[1,2,2,np.pi/2]])  
   # CZ pi/2
 
   # Meassure

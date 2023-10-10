33626d99c38c3f2f2838d2c781933cd6917fb19f
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 02:35:01 +0200

    Removed DQN Baseline

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,7 +1,6 @@
 from .a2c import A2C
-from .dqn import DQN
 from .ppo import PPO
 from .sac import SAC
 from .td3 import TD3
 
-ALGS = ['A2C', 'PPO', 'SAC', 'DQN', 'TD3']
+ALGS = ['A2C', 'PPO', 'SAC', 'TD3']

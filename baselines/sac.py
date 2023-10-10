f6bb55a36f5e4276d455776408a147a02115a61f
Philipp <philipp.altmann@ifi.lmu.de>
2023-10-10 02:35:38 +0200

    Updated SAC

--- baselines/sac.py
+++ baselines/sac.py
@@ -4,5 +4,3 @@ from stable_baselines3.sac import SAC as StableSAC
 class SAC(TrainableAlgorithm, StableSAC):
   """A Trainable extension to SAC"""
   n_steps = 1 # For unified logging
-  def __init__(self, **kwargs):
-    super().__init__(envkwargs={'discrete': False}, **kwargs)

94802a6ff8889222667488eb062b7e13d9a407e0
Philipp <philipp.altmann@ifi.lmu.de>
2024-04-22 16:27:13 +0200

    Update setup, bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-train = [ "stable_baselines3>=2.0.0", "tqdm>=4.65.0"]
+train = [ "torch==2.0.1", "stable_baselines3>=2.0.0", "tqdm>=4.65.0"]
 plot = ["plotly>=5.0", "tensorboard>=2.0"]
 
 setup(
-  name="qcd-gym", version="0.1.0",
+  name="qcd-gym", version="0.2.0",
   description="Quantum Circuit Designer: A gymnasium-based set of environments for benchmarking reinforcement learning for quantum circuit design.",
   url="https://github.com/philippaltmann/qcd", author_email="philipp@hyphi.co", license="MIT",
   keywords="benchmark reinforcement-learning quantum-computing gymnasium circuit-design",
   long_description=(Path(__file__).parent / "README.md").read_text(), long_description_content_type="text/markdown",
   packages=[package for package in find_packages() if package.startswith("circuit_designer")],
-  install_requires=[
-    "gymnasium==0.29",              # Bump to 1.0 once available 
-    "pennylane-lightning==0.32.0",  # Quantum Simulation (including pennylane base)
-  ],
+  install_requires=[ "gymnasium==0.29", "qiskit==1.0.2" ],
   extras_require = { 
     "tests": [ "pytest", "black"], 
     "train": train, "plot": plot,

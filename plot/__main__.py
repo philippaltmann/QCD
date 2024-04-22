32349fcae06420b92c72fd9e5c983828710aed27
Philipp <philipp.altmann@ifi.lmu.de>
2024-04-22 16:18:47 +0200

    Updated plotting

--- plot/__main__.py
+++ plot/__main__.py
@@ -7,22 +7,24 @@ options = { # Title, Scalar(Name, Tag), process(scalar)->data, display(data)->tr
   'Return': (('Return', 'rewards/return-100-mean'), process_ci, plot_ci),
   'Qubits': (('Qubits', 'rewards/qbits-100-mean'), process_ci, plot_ci),
   'Depth': (('Depth', 'rewards/depth-100-mean'), process_ci, plot_ci),
+  'Metric': (('Metric', 'rewards/metric-100-mean'), process_ci, plot_ci),
+  'Cost': (('Cost', 'rewards/cost-100-mean'), process_ci, plot_ci),
 }
 
 # Process commandline arguments 
 parser = argparse.ArgumentParser()
 parser.add_argument('base', help='The results root')
 parser.add_argument('-a', dest='alg', nargs='+', help='The algorithm to vizualise') #choices=[*ALGS]
-parser.add_argument('-r', dest='random_baseline', action='store_true')
+parser.add_argument('-b', dest='baseline', action='store_true')
 parser.add_argument('-e', dest='env', help='Environment to vizualise.')
 parser.add_argument('-g', dest='groupby', nargs='+', default=['env'], metavar="groupby", help='Experiment keys to group plotted data by.')
-parser.add_argument('-m', dest='metrics', nargs='+', default=['Return', 'Qubits', 'Depth'], choices=options.keys(), help='Experiment keys to group plotted data by.')
+parser.add_argument('-m', dest='metrics', nargs='+', default=['Qubits', 'Depth', 'Metric'], choices=options.keys(), help='Experiment keys to group plotted data by.')
 parser.add_argument('--mergeon', help='Key to merge experiments e.g. algorithm.')
 parser.add_argument('--no-dump', dest='dump_csv', action='store_false', help='Skip csv dump')
 
 args = vars(parser.parse_args()); tryint = lambda s: int(s) if s.isdigit() else s
 if args['alg']: args['alg'] = ' '.join(args['alg'])
-groupby = args.pop('groupby'); mergeon = args.pop('mergeon'); random_baseline = args.pop('random_baseline')
+groupby = args.pop('groupby'); mergeon = args.pop('mergeon'); baseline = args.pop('baseline')
 mergemetrics,_ = (True, groupby.remove('metrics')) if 'metrics' in groupby else (False,None)
 
 metrics = [(metric, *options[metric]) for metric in args.pop('metrics')]
@@ -30,7 +32,10 @@ titles, scalars, procs, plotters = zip(*metrics)
 
 experiments = fetch_experiments(**args, metrics=list(zip(titles, scalars)))
 experiments = group_experiments(experiments, groupby, mergeon)
-if random_baseline: [fetch_random(args['base'], exp) for exp in experiments]
+if baseline: 
+  [fetch_evo(args['base'], exp, dump=args['dump_csv']) for exp in experiments]
+  [fetch_random(args['base'], exp, dump=args['dump_csv']) for exp in experiments]
+
 experiments = calculate_metrics(experiments, list(zip(titles, procs)))
 if mergemetrics:
   experiments = [ {'title': t, 'metric': metrics[0][0], 'merge': True, 

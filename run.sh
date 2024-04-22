346e12dd4d39a835ab9a663be85e4a3f61e3c9bc
Philipp <philipp.altmann@ifi.lmu.de>
2024-04-22 16:25:46 +0200

    Updated training runners

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
-for ITERATION in 0 2 4 6; do
+BASE='results'
+for ITERATION in 0 2 4 6; do #0 2 4 6
   for RUN in 1 2; do  # Depth = 3xoptimal solution
-    for CHALLENGE in 'UC-hadamard-q1-d9' 'SP-bell-q2-d12' 'SP-ghz3-q3-d15' 'UC-random-q2-d12' 'SP-random-q2-d12' 'UC-toffoli-q3-d63'; do
-      for ALG in 'A2C' 'PPO' 'SAC' 'TD3'; do
+    for TASK in 'UC-hadamard-q1-d9' 'SP-random-q2-d12'; do # Base 
+    # for TASK in 'SP-ghz3-q3-d15'  'UC-toffoli-q3-d63'; do # Advanced
+    # for TASK in 'SP-bell-q2-d12' 'UC-random-q2-d12'; do # Additional 
+      for ALG in 'PPO' 'SAC' 'A2C' 'TD3'; do
         SEED=$(($ITERATION + $RUN))
-        O="results/out/$CHALLENGE/$ALG"; mkdir -p "$O"
-        echo "Running $ALG in $CHALLENGE [SEED $SEED]"
-        python -m train $ALG -e $CHALLENGE -s $SEED &> "$O/$SEED.out" &
+        O="$BASE/out/$TASK/$ALG"; mkdir -p "$O"
+        echo "Running $ALG in $TASK [SEED $SEED]"
+        python -m train $ALG -e $TASK -s $SEED --sparse --punish --path $BASE &> "$O/$SEED.out" &
         sleep 5 
       done
     done
--- train/__main__.py
+++ train/__main__.py
@@ -5,35 +5,35 @@ start = time.time()
 # General Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('method', help='The algorithm to use', choices=[*ALGS])
-parser.add_argument( '-e', dest='envs', nargs='+', default=['Maze7Target'], metavar="Environment", help='The name and spec and of the safety environments to train and test the agent. Usage: --env NAME, CONFIG, N_TRAIN, N_TEST')
+parser.add_argument( '-e', dest='env', metavar="Environment")
 
 parser.add_argument('-s', dest='seed', type=int, help='The random seed. If not specified a free seed [0;999] is randomly chosen')
 parser.add_argument('-t', dest='timesteps', type=int, help='The number of timesteps to explore.', default=128*(2048*4)) #~10e5
 parser.add_argument('--load', type=str, help='Path to load the model.')
 parser.add_argument('--test', help='Run in test mode (dont write log files).', action='store_true')
-parser.add_argument('--stop', dest='stop_on_reward', help='Stop at reward threshold.', action='store_true') # TODO: test
 parser.add_argument('--path', default='results', help='The base path, defaults to `results`')
-# parser.add_argument('-d',  dest='device', default='cuda', choices=['cuda','cpu'])
+parser.add_argument('--punish', action='store_true')
+parser.add_argument('--sparse', action='store_true')
 
 # Get arguments & extract training parameters & merge model args
 args = {key: value for key, value in vars(parser.parse_args()).items() if value is not None}; 
 if args.pop('test'): args['path'] = None
+args['envkwargs'] = {'punish': args.pop('punish'), 'sparse': args.pop('sparse')}
 timesteps = args.pop('timesteps'); presteps = 0
 load = args.pop('load', None)
 
 # Init Training Model
 trainer = eval(args.pop('method'))
-# args['policy'] = 'MultiInputPolicy'
  
 model = trainer(**args)
-model._naming = {**model._naming , 'd': 'depth-100', 'q': 'qbits-100', 'o': 'ops-100'}
+model._naming = {**model._naming , 'd': 'depth-100', 'q': 'qbits-100', 'o': 'ops-100', 'm': 'metric-100', 'c': 'cost-100'}
 
 if load is not None:
   _params = model.get_parameters()['policy'].copy().__str__()
   model.set_parameters({**model.get_parameters(), 'policy': model.policy.load(load).state_dict()}) # v2: load policy from file
   assert _params != model.get_parameters()['policy'].copy().__str__(), "Load failed"
 
-print(f"Training {trainer.__name__ } in {model.envs['train'].envs[0].unwrapped.name} for {timesteps-presteps:.0f} steps.") 
-model.learn(total_timesteps=timesteps-presteps) #, reset_num_timesteps = not pretrain
+print(f"Training {trainer.__name__ } in {args['env']} for {timesteps-presteps:.0f} steps.") 
+model.learn(total_timesteps=timesteps-presteps) 
 if model.path: model.save()
 print(f"Done in {time.time()-start}")

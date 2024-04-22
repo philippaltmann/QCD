c51475793c873142640d33e42c7de173982a15db
Philipp <philipp.altmann@ifi.lmu.de>
2024-04-22 16:22:13 +0200

    Fixed multi-registration

--- circuit_designer/__init__.py
+++ circuit_designer/__init__.py
@@ -1,4 +1,5 @@
-from gymnasium.envs.registration import register
+from gymnasium.envs.registration import register, registry
 
 def register_envs(): 
+  if "CircuitDesigner-v0" in registry: return
   register(id="CircuitDesigner-v0", entry_point="circuit_designer.env:CircuitDesigner")

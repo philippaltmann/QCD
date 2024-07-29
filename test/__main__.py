944e9ac2948aac9f6f16aa0814fcc7123eea2f39
Philipp <philipp.altmann@ifi.lmu.de>
2024-07-29 12:34:06 +0200

    Refactored Tests

---
+++ test/__main__.py
@@ -0,0 +1,12 @@
+from test.bell import bell
+from test.ghz import ghz
+from test.hadamard import hadamard
+from test.toffoli import toffoli
+
+# State Preparation
+bell()
+ghz()
+
+# Unitary Composition
+hadamard()
+toffoli()
